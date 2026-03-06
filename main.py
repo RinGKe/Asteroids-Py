33341ab99bb0ba676ba34cafe3d61fdf7e366f9b
RinGke <keaganspires@gmail.com>
2026-03-06 02:46:49 +0000

    fixes

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.init()
+    pygame.display.init()
     pygame.font.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
